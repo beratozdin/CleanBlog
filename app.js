ac37c4776bbe433ac58715bce2ed2a8a0712380b
beratozdin <berat.ozdin2016@gtu.edu.tr>
2021-05-28 11:57:38 +0300

    Homework part 2 is done

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
-
+const path=require('path');
+const ejs=require('ejs');
 const app = express();
 
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-
-const blog = { id: 1, 
-    title: "Blog title", 
-    description: "Blog description" }
-  res.send(blog)
-});
-
+    res.render('index');
+  })
+  
+  app.get('/about', (req, res) => {
+    res.render('about');
+  })
+  
+  app.get('/add', (req, res) => {
+    res.render('add');
+  })
+  
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
